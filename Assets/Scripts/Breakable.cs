b145b0dff67d926331888b1bb7b0c8c118bc1c62
Calex Raffield <clxrffdman@gmail.com>
2023-08-25 11:32:35 -0700

    - ui setup + breakable setup

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    //base class for all physics breakables
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
