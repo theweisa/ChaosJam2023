21f24a6f478a0bcc09ff93ea29e47931e9793fa8
Calex Raffield <clxrffdman@gmail.com>
2023-08-25 14:17:08 -0700

    -virtual camera setup

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraManager : UnitySingleton<CameraManager>
 {
     public CameraState currentCameraState = CameraState.StageView;
+
+    [SerializeField] private CinemachineVirtualCamera playerCamera, collisionCamera, stageCamera;
 
     private void UpdateCameraFromState()
     {
