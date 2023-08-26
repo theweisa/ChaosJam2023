8102471e00c8009557a6f07f4d843dbf4e947f94
theweisa <weiandrew2012@gmail.com>
2023-08-25 19:57:27 -0700

    aaa

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class PlayerManager : UnitySingleton<PlayerManager>
 {
-    public Rigidbody2D ratRb;
-    public bool ratHeld = false;
-    public bool ratThrown = false;
+    public GameObject currentRat, tail;
+    public bool isHeld = false;
+    public float stopVelocity = 0.2f;
     // Start is called before the first frame update
     public override void Awake()
     {
         base.Awake();
-        ratRb = ratRb != null ? ratRb : Global.FindComponent<Rigidbody2D>(gameObject);
+    }
+
+    public IEnumerator ThrowRat() {
+        isHeld = false;
+        GameManager.Instance.gameState = GameManager.GameState.Thrown;
+        currentRat.GetComponent<RatClamp>().ignoreClamp = true;
+
+        CameraManager.Instance.PanToCamera(CameraManager.Instance.collisionCamera);
+        yield return new WaitUntil(() => currentRat.GetComponent<Rigidbody2D>().velocity.magnitude <= stopVelocity);
+        if (GameManager.Instance.gameState != GameManager.GameState.Win) {
+            ResetRat();
+        }
+    }
+
+    public void ResetRat() {
+        isHeld = true;
+        GameManager.Instance.gameState = GameManager.GameState.Throwing;
+
     }
 
     // Update is called once per frame
